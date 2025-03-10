418ad5a56f7b1090b6adeebe5c69b04f735751d2
unknown <chamindugangana2001@gmail.com>
2025-03-10 10:45:53 +0530

    Initial

---
+++ Scanner2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Scanner2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enater Name = ");
+
+        String Name = input.nextLine();
+        System.out.print("Your Name = " + Name);
+
+    }
+    
+}
